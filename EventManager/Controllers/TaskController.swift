1d3a96e1f8e6485c7be8d659fe70e3affe7da065
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-04 14:09:56 -0800

    add all files created with folder structure

---
+++ EventManager/Controllers/TaskController.swift
@@ -0,0 +1,8 @@
+//
+//  TaskController.swift
+//  EventManager
+//
+//  Created by Mehdi MMS on 04/03/2022.
+//
+
+import Foundation
---
+++ EventManager/Controllers/ViewControllers/TaskDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskDetailViewController.swift
+//  EventManager
+//
+//  Created by Mehdi MMS on 04/03/2022.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
