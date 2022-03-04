7b4ba04686f85325bdec510d44627163363d3441
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-04 14:04:06 -0800

    Initial Commit

---
+++ EventManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventManager
+//
+//  Created by Mehdi MMS on 04/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
